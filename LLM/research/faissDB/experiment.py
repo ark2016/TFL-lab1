d3dc6828ad503f911b649799b1fc88120cac371d
Ark <A6611688@yandex.ru>
2024-11-27 13:13:54 +0300

    upd

--- LLM/research/faissDB/experiment.py
+++ LLM/research/faissDB/experiment.py
@@ -12,3 +12,4 @@ with open(file_path, 'r', encoding='utf-8') as file:
 for i in range(0, len(data)):
     question = [{"question": data[i]['question'], "answer": ""}]
     print(process_questions(question, use_saved=True, filename="new_database"))
+    print("\n\n\n\n")
