21584003b792d2b3450d185c2ee0ffb206df9813
Ark <A6611688@yandex.ru>
2024-12-09 21:53:44 +0300

    upd some

--- LLM/research/faissDB/main.py
+++ LLM/research/faissDB/main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     s = 1
 
     # Оценка качества работы алгоритма
-    for k_max in [5, 3, 2, 1]:
-        for similarity_trashold in [0.8, 0.75, 0.72, 0.7, 0.65, 0.6]:
+    for similarity_trashold in [0.75, 0.73, 0.72, 0.71, 0.69, 0.68, 0.67, 0.66, 0.65]:
+        for k_max in [3, 2]:
             top_k_accuracy, jaccard_index = evaluate_algorithm(dataset_path, db_path, k, s, k_max=k_max,
                                                                similarity_trashold=similarity_trashold)
             print(f"\n\nk_max: {k_max}, similarity_trashold: {similarity_trashold}")
